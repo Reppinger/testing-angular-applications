69d9af3bdb0d311bfa806c6dc6a4277f3d58bf0e
Randy Eppinger <r@eppingers.name>
2018-12-02 15:08:47 -0500

    saveContact() test passing. I am utterly aghast. This is a ridiculous amount of code for one test.  I know that some of the stubs, etc. were built in anticipation of future tests. However, this is horrible at this point.

--- website/src/app/contacts/contact-edit/contact-edit.component.spec.ts
+++ website/src/app/contacts/contact-edit/contact-edit.component.spec.ts
@@ -12,5 +12,67 @@ import '../../../material-app-theme.scss';
 
 
 describe('ContactEditComponent tests', () => {
+    let fixture: ComponentFixture<ContactEditComponent>;
+    let component: ContactEditComponent;
+    let contactService: ContactService;
+    let rootElement: DebugElement;
+    const contactServiceStub = {
+        contact: {
+            id: 1,
+            name: 'janet'
+        },
+        save: async function (contact: Contact) {
+            component.contact = contact;
+        },
+        getContact: async function () {
+            component.contact = this.contact;
+            return this.contact;
+        },
+        updateContact: async function (contact: Contact) {
+            component.contact = contact;
+        }
+    };
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ContactEditComponent, FavoriteIconDirective,
+                InvalidEmailModalComponent, InvalidPhoneNumberModalComponent],
+            imports: [
+                AppMaterialModule,
+                FormsModule,
+                NoopAnimationsModule,
+                RouterTestingModule
+            ],
+            providers: [{provide: ContactService, useValue: contactServiceStub}]
+        });
+        TestBed.overrideModule(BrowserDynamicTestingModule, {
+            set: {
+                entryComponents: [InvalidEmailModalComponent, InvalidPhoneNumberModalComponent]
+            }
+        });
+        contactService = TestBed.get(ContactService);
+    });
+    beforeEach(() => {
+            fixture = TestBed.createComponent(ContactEditComponent);
+            component = fixture.componentInstance;
+            fixture.detectChanges();
+            rootElement = fixture.debugElement;
+        }
+    );
+
+    describe('saveContact() test', () => {
+        it('should cause contact name display', fakeAsync(() => {
+            const contact = {
+                id: 1,
+                name: 'lorace'
+            };
+            component.isLoading = false;
+            component.saveContact(contact);
+            fixture.detectChanges();
+            const nameInput = rootElement.query(By.css('.contact-name'));
+            tick();
+            expect(nameInput.nativeElement.value).toBe('lorace');
+        }));
+    });
 
 });
