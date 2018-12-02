5c5b3abdf41ecf2266dd27d172b1a8be56af4f03
Randy Eppinger <r@eppingers.name>
2018-12-02 14:23:17 -0500

    More imports. Honestly, this is ridiculous. There has to be a better way.

--- website/src/app/contacts/contact-edit/contact-edit.component.spec.ts
+++ website/src/app/contacts/contact-edit/contact-edit.component.spec.ts
@@ -1,10 +1,16 @@
-import { DebugElement} from '@angular/core';
-import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { NoopAnimationsModule} from '@angular/platform-browser/animations';
-import { BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
+import {DebugElement} from '@angular/core';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
 import {RouterTestingModule} from '@angular/router/testing';
+import {FormsModule} from '@angular/forms';
+import {Contact, ContactService, FavoriteIconDirective, InvalidPhoneNumberModalComponent, InvalidEmailModalComponent} from '../shared';
+import {AppMaterialModule} from '../../app.material.module';
+import {ContactEditComponent} from './contact-edit.component';
+import '../../../material-app-theme.scss';
 
-describe ('ContactEditTests', () => {
+
+describe('ContactEditComponent tests', () => {
 
 });
