d436b0979c06de9ed5c91254dfb549f45787c07d
Randy Eppinger <r@eppingers.name>
2018-12-02 16:12:26 -0500

    Removed what I consider to be unnecessary setting of contact when it has a default. Test shorter and still works.

--- website/src/app/contacts/contact-edit/contact-edit.component.spec.ts
+++ website/src/app/contacts/contact-edit/contact-edit.component.spec.ts
@@ -94,17 +94,11 @@ describe('ContactEditComponent tests', () => {
                email: 'london@example.com',
                number: '1234567890'
            };
-           component.contact = {
-               id: 2,
-               name: 'chauncey',
-               email: 'chauncey@example.com',
-               number: '1234567890'
-           };
            component.isLoading = false;
            fixture.detectChanges();
            const nameInput = rootElement.query(By.css('.contact-name'));
            tick();
-           expect(nameInput.nativeElement.value).toBe('chauncey');
+           expect(nameInput.nativeElement.value).toBe('janet');
            component.updateContact(newContact);
            fixture.detectChanges();
            tick(100);
